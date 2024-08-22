f68c28e78d2e193141b3242c7156440dfbf6c152
Tatsiana <badpanda@tut.by>
2024-08-22 19:43:40 +0200

    Props name, salary and increase are added to project

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -5,6 +5,13 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
+const data = [
+    {name: 'John L', salary: 800, increase: true},
+    {name: 'Lisa T', salary: 1000, increase: false},
+    {name: 'Melody W', salary: 1400, increase:true}
+]
+
+
 function App() {
     return (
         <div className="app">
@@ -12,9 +19,10 @@ function App() {
             <div className="search-panel">
             <SearchPanel/>
             <AppFilter/>
-            <EmployeesList/>
-            <EmployeesAddForm/>
             </div>
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+           
         </div>
     );
 }
