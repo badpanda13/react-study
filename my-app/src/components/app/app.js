f1d507e9c3fb63449a87c6caa75c25bcda5c9151
Tatsiana <badpanda@tut.by>
2024-08-23 17:21:01 +0200

    Remove item from list by id

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,26 +7,42 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
-const data = [
-    {name: 'John L', salary: 800, increase: true, id:1},
-    {name: 'Lisa T', salary: 1000, increase: false, id:2},
-    {name: 'Melody W', salary: 1400, increase:true, id:3}
-]
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : [
+                {name: 'John L', salary: 800, increase: true, id:1},
+                {name: 'Lisa T', salary: 1000, increase: false, id:2},
+                {name: 'Melody W', salary: 1400, increase:true, id:3}
+            ]
+        }
+    }
 
+    deleteItem =(id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !==id)  
+            }
+        })
+    }
 
-function App() {
-    return (
-        <div className="app">
-            <AppInfo/>
-            <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+                <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+                </div>
+                <EmployeesList 
+                data={this.state.data}
+                onDelete={this.deleteItem}/>
+                <EmployeesAddForm/>
+            
             </div>
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-           
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
